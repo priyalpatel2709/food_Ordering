a4f6ba4a3ce5ad931b88624758a950c462d5cd19
priyal patel <piyu9552@gmail.com>
2025-01-25 21:21:39 +0530

    remove ts :(

--- src/controllers/userControllers.ts
+++
@@ -1,6 +0,0 @@
-import { Request, Response } from "express";
-import asyncHandler from "express-async-handler";
-
-export const getAllUsers = asyncHandler(async (req: Request, res: Response) => {
-  res.send("hello form TS");
-});
--- src/routes/userRoute.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import {getAllUsers} from "../controllers/userControllers" ;
-
-const router = Router()
-
-router.get('/',getAllUsers)
-
-export default router;
